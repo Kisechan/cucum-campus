b354048c01263fa068945460e79d536ddab584aa
Kisechan <kisechan_@outlook.com>
2025-05-01 14:56:31 +0800

    第一问修改：调整参数

--- q1.py
+++ q1.py
@@ -21,37 +21,47 @@ def Calc_N_outdoor():
     N_outdoor = A_EFF / S_MIN * T_OPEN / T_STAY_OUT
     return N_outdoor
 
-MU = 10             # 服务率(人/小时)
-C_SERVICE = 6       # 服务台数量
-Wq_LIMIT = 0.25     # 排队时间限制
+# 三类服务点：休息点，商服点，医疗点
+MU = [320, 200, 40]             # 服务率(人/小时)
+C_SERVICE = [12, 6, 3]          # 服务台数量
+P_SERVICE = [0.3, 0.1, 0.01]    # 服务点游客比例
+Wq_LIMIT = 0.30                 # 排队时间限制(小时)
 
-def Calc_Wq(lmbd):
-    rho = lmbd / (C_SERVICE * MU)
-    if rho >= 1 or rho <= 0:
-        return float('inf')
-        # 系统不稳定或数据出错
-    Wq = ((lmbd / MU) ** C_SERVICE) / (math.factorial(C_SERVICE) * ((1 - rho) ** 3) * C_SERVICE * MU)
-    return Wq
 
-def Get_lambda():
+# 计算单类商服点的 λ
+def Calc_lambda_max(mu, c):
+    def Calc_Wq(lmbd):
+        rho = lmbd / (c * mu)
+        if rho >= 1 or rho <= 0:
+            return float('inf')
+            # 系统不稳定或数据出错
+        Wq = ((lmbd / mu) ** c) / (math.factorial(c) * ((1 - rho) ** 3) * c * mu)
+        return Wq
+
+    # 二分法求解λ_max
     tolerance = 1e-6
-    l = 0.0
-    r = C_SERVICE * MU - tolerance
+    l, r = 0.0, c * mu - tolerance
     lmbd = 0.0
     while r - l > tolerance:
         mid = l + (r - l) / 2
-        Wq = Calc_Wq(mid)
-        if Wq < Wq_LIMIT:
+        if Calc_Wq(mid) <= Wq_LIMIT:
             l = mid
             lmbd = mid
         else:
             r = mid
     return lmbd
 
+# 日服务能力
 def Calc_N_service():
-    lmbd = Get_lambda()
-    N_service = lmbd * T_OPEN
-    return N_service
+    N_service_list = [(Calc_lambda_max(mu, c) / p) for mu, c, p in zip(MU, C_SERVICE, P_SERVICE)]
+
+    print(f'N_service_list = {[f"{x:.1f}人" for x in N_service_list]}')
+
+    weights = np.array(C_SERVICE) / sum(C_SERVICE)
+
+    # 取三种服务点的加权平均返回
+    return np.sum(np.array(N_service_list) * weights)
+
 
 V = 1.2             # 人群移动速度
 W = 12              # 出口宽度
@@ -65,11 +75,14 @@ def Calc_t_escape():
 
 def Solve():
     # 权重
-    omega1 = 0.8
-    omega2 = 0.2
+    omega1 = 0.1
+    omega2 = 1 - omega1
+
     N_indoor = Calc_N_indoor()
     N_service = Calc_N_service()
     N_escape = V * W * Calc_t_escape() / T_STAY_OUT
+
+    print(f'N_indoor: {N_indoor:.1f}人, N_service: {N_service:.1f}人, N_escape: {N_escape:.1f}人')
 
     N_upper = min(N_indoor, N_service, N_escape)
 
